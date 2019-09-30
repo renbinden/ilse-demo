80bf07dd09678480ae328524393c210267c7cde2
Ren Binden <ren.binden@gmail.com>
2019-09-30 23:13:59 +0100

    Update to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("org.jetbrains.kotlin.js") version "1.3.50"
 }
 
-group = "ilse-test"
-version = "1.0.0-SNAPSHOT"
+group = "uk.co.renbinden"
+version = "1.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
-    implementation(group = "uk.co.renbinden", name = "ilse", version = "1.0.0")
+    implementation(group = "uk.co.renbinden", name = "ilse", version = "1.1.0")
     testCompile(group = "junit", name = "junit", version = "4.12")
     testImplementation("org.jetbrains.kotlin:kotlin-test-js")
 }
